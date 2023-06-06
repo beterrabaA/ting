8a2a0fbedbee7882107ff369018beb0a6350e81f
Lucas Vieira <ls6182315@gmail.com>
2023-06-06 22:38:38 +0000

    feat: process  function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+import sys
+""'Importa sys para gerar erros'""
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            sys.stdout.write(f"Arquivo {path_file} já está na fila\n")
+
+    text = txt_importer(path_file)
+
+    if text is None:
+        sys.stdout.write(f"Arquivo {path_file} não encontrado\n")
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text),
+        "linhas_do_arquivo": text
+    }
+
+    instance.enqueue(data)
+    sys.stdout.write(str(data))
 
 
 def remove(instance):
