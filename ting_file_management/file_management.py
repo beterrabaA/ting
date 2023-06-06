ef20101e7e36f20bf59307c0ca931fd7f0e6fc99
Lucas Vieira <ls6182315@gmail.com>
2023-06-06 22:37:55 +0000

    feat: text_importer function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+""'Importa sys para gerar erros de arquivo não encontrado'""
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    ""'Importa um arquivo txt e retorna seu conteúdo'""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write("Formato inválido")
+    try:
+
+        with open(path_file, 'r', encoding='utf-8') as file:
+            return [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
